a4356a1af6424d10bf991d9f89e5f825e88521e0
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 07:47:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 var katzDeli = [];
 
-takeANumber = () => {
-  return null
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine.length == 0)
+  {
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var first = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${first}.`;
+  }
+}
+
+
+function currentLine(katzDeliLine)
+{
+  var result = "The line is currently: ";
+  for(var i = 0; i < katzDeliLine.length; i++)
+  {
+    result += `${i + 1}. ${katzDeliLine[i]}${i == katzDeliLine.length - 1 ? "" : ", "}`
+  }
+  return katzDeliLine.length == 0 ? "The line is currently empty." : result
 }
