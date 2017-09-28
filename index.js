8bf7605e8beec1340e9be2f2d5e97d31c41aa509
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 07:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine.length == 0)
+  {
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var first = katzDeliLine[0];
+    katzDeliLine.shift();
+    return first;
+  }
+}
